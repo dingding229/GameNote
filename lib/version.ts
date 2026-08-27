2db84481c2b79a0f944a516c4d5720552443897d
ding ding <dingding@DingDings-MacBook-Air.local>
2026-08-28 00:50:02 +0800

    chore: release v1.0.11

--- lib/version.ts
+++ lib/version.ts
@@ -1,4 +1,4 @@
-export const appVersion = "1.0.10";
+export const appVersion = "1.0.11";
 
 export function normalizeVersion(value: string) {
   const match = value.trim().match(/^v?(\d+)\.(\d+)\.(\d+)$/i);
